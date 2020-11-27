b975af0e7ec7f1dbafef90904fc70055320e73cc
Cyril <cyril.cordier@epitech.eu>
2020-11-27 12:26:16 +0100

    Updates

---
+++ precache-manifest.f0fd95fe2d14e6e771d3e49c6825bfc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abe6529ba73b27ad1a6b2cb7cfb3a5f",
+    "url": "/Redux-React/index.html"
+  },
+  {
+    "revision": "cb3b1d9a64d5fa274e6c",
+    "url": "/Redux-React/static/css/main.ca6b460a.chunk.css"
+  },
+  {
+    "revision": "bb15094cde0b53e2ee51",
+    "url": "/Redux-React/static/js/2.6d2318b8.chunk.js"
+  },
+  {
+    "revision": "c50c771658c38c7c1fcfc5b118622f46",
+    "url": "/Redux-React/static/js/2.6d2318b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb3b1d9a64d5fa274e6c",
+    "url": "/Redux-React/static/js/main.9bef247c.chunk.js"
+  },
+  {
+    "revision": "2dca644f96b9eb5b8dca",
+    "url": "/Redux-React/static/js/runtime-main.c0551e82.js"
+  }
+]);
